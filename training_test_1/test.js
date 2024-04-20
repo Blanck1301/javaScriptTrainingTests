eb10d6aa0b884e4bcb925e70892cb72a46b5bbec
dani <dblanc1301@gmail.com>
2024-04-20 17:00:18 +0200

    haha

--- training_test_1/test.js
+++ training_test_1/test.js
@@ -1,3 +1,23 @@
 //comentario en javascript
-let tupu=23;
-let tamadre=44;
+
+
+let tupu=prompt("dame un número pls:");
+tupu= parseInt(tupu);
+let tamadre=prompt("dame un número pls:");
+tamadre= parseInt(tamadre);
+
+
+function sumaMadres(x, y) {
+    let z =x+y;
+    return z;
+    }
+let z =sumaMadres(tupu,tamadre);
+console.log(z);
+/* comentario multilínea en javaScript
+ves?
+ves?
+ves?
+*/
+if(z>70){
+    console.log("este número es mayor que 70, no me agrada")
+}
